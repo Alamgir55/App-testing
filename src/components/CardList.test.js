f437ab4b1cb45d1bca67445cb56f0d4f94bf5b79
alamgir hossain <alamgir555hossain@gmail.com>
2020-03-14 19:03:55 -0400

    tested on App.js

---
+++ src/components/CardList.test.js
@@ -0,0 +1,17 @@
+import { shallow } from 'enzyme';
+import React from 'react';
+import CardList from './CardList';
+
+it('expect to render CardList component', () => {
+    const mockRobots = [
+        {
+            id: 1,
+            name: 'John snow',
+            username: 'johnjohn',
+            email: 'johnsnow@gmail.com'
+
+        }
+    ];
+
+    expect(shallow(<CardList robots={mockRobots} />)).toMatchSnapshot();
+});
